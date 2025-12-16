0b35c24f82ceae3736f185b56fbf73f456261ad6
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-12-17 01:04:49 +0300

    Add MongoDB connection and dependency
    Introduced the connectDB module and integrated MongoDB connection in app.js. Added the 'mongodb' package to dependencies. Also added VSCode spell check settings.

--- starter/app.js
+++ starter/app.js
@@ -17,6 +17,9 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 app.use(express.json());
 // extra packages
 
+// connectDB
+const connectDB = require('./db/connect');
+
 // routes
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/jobs',jobsRouter);
@@ -28,6 +31,8 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
+    console.log('database connected')
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
