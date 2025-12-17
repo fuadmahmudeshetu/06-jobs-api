8adb5cc808c9e87d8b4739109774a4c880ac891d
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-12-18 00:45:59 +0300

    response name and token

--- starter/controllers/auth.js
+++ starter/controllers/auth.js
@@ -9,7 +9,7 @@ const register = async (req,res) => {
 
     const token = jwt.sign({userId:user._id, name:user.name}, 'jwtSecret', { expiresIn: '30d'})
 
-    res.status(StatusCodes.CREATED).json(token)
+    res.status(StatusCodes.CREATED).json({user:{name:user.name}, token})
 }
 
 const login = async (req,res) =>{
