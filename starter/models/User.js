61864a3c3420322f00b4bbacab9e5cfb3315bf9b
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-12-17 11:31:49 +0300

    Add password field and unique email to User model
    Introduces a password field with validation to the User schema and enforces uniqueness on the email field. Also adds module export for the User model.

--- starter/models/User.js
+++ starter/models/User.js
@@ -14,6 +14,15 @@ const UserSchema = new mongoose.Schema({
         match: [
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
             'please provide a valid email'
-        ]
+        ],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide name'],
+        minlength: 6,
+        maxlength: 12,
     }
 })
+
+module.exports = mongoose.model('User', UserSchema)
