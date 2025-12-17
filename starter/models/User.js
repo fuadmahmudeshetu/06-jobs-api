44759564b571d0ba490cecb30c9312fb9bd2b469
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-12-17 23:18:45 +0300

    hash password in the model

--- starter/models/User.js
+++ starter/models/User.js
@@ -1,9 +1,9 @@
-const { required } = require('joi')
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
     name: {
-        type:String,
+        type: String,
         required: [true, 'Please provide name'],
         minlength: 3,
         maxlength: 50,
@@ -13,15 +13,22 @@ const UserSchema = new mongoose.Schema({
         required: [true, 'Please provide email'],
         match: [
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-            'please provide a valid email'
+            'Please provide a valid email',
         ],
-        unique: true
+        unique: true,
     },
     password: {
         type: String,
-        required: [true, 'Please provide name'],
-        minlength: 6
-    }
+        required: [true, 'Please provide password'],
+        minlength: 6,
+    },
+})
+
+UserSchema.pre('save', async function () {
+    if (!this.isModified('password')) return
+
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
 })
 
 module.exports = mongoose.model('User', UserSchema)
