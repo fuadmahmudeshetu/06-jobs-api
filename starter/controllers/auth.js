8d47ed8e39ef016d913e21e4fd375833a57398c5
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-12-18 00:24:51 +0300

    setup the toke for user register

--- starter/controllers/auth.js
+++ starter/controllers/auth.js
@@ -1,11 +1,15 @@
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
 const {BadRequestError} = require('../errors')
-const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const register = async (req,res) => {
+
     const user = await User.create({...req.body})
-    res.status(StatusCodes.CREATED).json(user)
+
+    const token = jwt.sign({userId:user._id, name:user.name}, 'jwtSecret', { expiresIn: '30d'})
+
+    res.status(StatusCodes.CREATED).json(token)
 }
 
 const login = async (req,res) =>{
