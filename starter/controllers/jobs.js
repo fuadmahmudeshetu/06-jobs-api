e43e4f3002c37829a9b9ac4de8dec0cfceaf01bc
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-12-19 00:20:14 +0300

    get single job

--- starter/controllers/jobs.js
+++ starter/controllers/jobs.js
@@ -3,19 +3,41 @@ const { StatusCodes } = require('http-status-codes')
 const { BadRequestError, NotFoundError } = require('../errors')
 
 const getAllJob = async (req, res) => {
-    res.send('get all jobs')
+    const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
+    res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
 }
 
 const getJob = async (req, res) => {
-    res.send('get job')
+    const {
+        user: { userId },
+        params: { id: jobId }
+    } = req
+
+    console.log('JOB ID:', jobId)
+    console.log('USER ID:', userId)
+
+    const job = await Job.findOne({ 
+        _id: jobId, 
+        createdBy: userId })
+
+    if (!job) {
+        throw new NotFoundError(`Job not found with id ${jobId}`)
+    }
+
+    res.status(StatusCodes.OK).json(
+        { job }
+    )
 }
 
 const createJob = async (req, res) => {
-    res.json(req.body)
+    req.body.createdBy = req.user.userId
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({ job })
 }
 
 const updateJob = async (req, res) => {
-    res.send('update job')
+    const {id:jobId} = req.params
+    
 }
 
 const deleteJob = async (req, res) => {
