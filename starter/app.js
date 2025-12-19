85d0aa6fd1dda885e0a96ea968bbbf144f38da8d
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-12-19 22:18:04 +0300

    add security packages

--- starter/app.js
+++ starter/app.js
@@ -1,5 +1,15 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages 
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
+
+
+
 const express = require('express');
 const app = express();
 
@@ -15,7 +25,18 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const authMiddleware = require('./middleware/authentication')
 
-app.use(express.json());
+app.set('trust proxy', 1);
+app.use(rateLimiter({
+  windowMs: 15*60*1000,
+  limit: 100,
+  standardHeaders: 'draft-8',
+  legacyHeaders: false,
+}))
+app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 // extra packages
 
 // connectDB
