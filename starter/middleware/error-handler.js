bbd84cf7edf17b0e02b8733642cc88a114e18be1
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-12-19 21:58:05 +0300

    done with error handler for jobs route

--- starter/middleware/error-handler.js
+++ starter/middleware/error-handler.js
@@ -11,17 +11,21 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   // }
 
   if (err.name === 'ValidationError') {
-    customError.msg = Object.values(err.errors).map((item)=> item.message).join(', ')
-
+    customError.msg = Object.values(err.errors).map((item) => item.message).join(', ')
     customError.statusCode = 400
   }
 
-  if (err.code && err.code === 11000 ) {
+  if (err.name === 'CastError') {
+    customError.msg = `No item found with the id ${err.value}`
+    customError.statusCode = 404
+  }
+
+  if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`
     customError.statusCode = 400
   }
   return res.status(customError.statusCode).json({ msg: customError.msg })
-  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
 }
 
 module.exports = errorHandlerMiddleware
